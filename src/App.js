40d05169c7ccc51b32f27179f46584cd8d0ee8cd
subin <stnqls3938@gmail.com>
2022-12-28 20:50:57 +0900

    React에 연결하기 1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import { addTodo } from "./redux/actions";
-import store from "./redux/store";
+// import { addTodo } from "./redux/actions";
+// import useReduxState from "./hooks/useReduxState";
+// import useReduxDispatch from "./hooks/useReduxDispatch";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
 
-function App({ store }) {
-  const [state, setState] = useState(store.getState());
+function App() {
+  // const store = useContext(ReduxContext);
 
-  useEffect(() => {
-    const unsubscribe = store.subscribe(() => {
-      setState(store.getState());
-    });
-    return () => {
-      unsubscribe();
-    };
-  }, [store]);
+  // const [state, setState] = useState(store.getState());
+
+  // useEffect(() => {
+  //   const unsubscribe = store.subscribe(() => {
+  //     setState(store.getState());
+  //   });
+  //   return () => {
+  //     unsubscribe();
+  //   };
+  // }, [store]);
+
+  // const state = useReduxState();
+  // const dispatch = useReduxDispatch();
 
   return (
     <div className="App">
-      {JSON.stringify(state)}
-      <button onClick={click}>추가</button>
+      {/* {JSON.stringify(state)} */}
+      {/* <button onClick={click}>추가</button> */}
+      <TodoList />
+      <TodoForm />
     </div>
   );
-}
 
-function click() {
-  store.dispatch(addTodo("HTML"));
+  // function click() {
+  //   // store.dispatch(addTodo("HTML"));
+  //   dispatch(addTodo("HTML"));
+  // }
 }
 
 export default App;
